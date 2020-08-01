01776f1bd5aac97ca6ec53c439d7a84a90076d8e
Oshi Gupta <oshiagupta36@gmail.com>
2020-08-01 16:42:05 +0530

    Updates

---
+++ precache-manifest.cd84f4e2814ab37e0bdc81ff1be9c3e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82af0bb860368aed2ae949e2f4408c96",
+    "url": "/To-do-app/index.html"
+  },
+  {
+    "revision": "ce00e0ba8fe2c2c76cb2",
+    "url": "/To-do-app/static/css/main.e8e88b99.chunk.css"
+  },
+  {
+    "revision": "170c4414bacb41027f8a",
+    "url": "/To-do-app/static/js/2.b6ac2b59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-app/static/js/2.b6ac2b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce00e0ba8fe2c2c76cb2",
+    "url": "/To-do-app/static/js/main.b8ad4a11.chunk.js"
+  },
+  {
+    "revision": "b4f2d459b609166b34d5",
+    "url": "/To-do-app/static/js/runtime-main.7654136d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-do-app/static/media/logo.5d5d9eef.svg"
+  }
+]);

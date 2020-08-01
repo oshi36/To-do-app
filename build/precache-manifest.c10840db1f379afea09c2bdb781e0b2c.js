bf00167b3bb6db9778a7bc9f7b12c359657559f2
oshi36 <47573417+oshi36@users.noreply.github.com>
2020-08-01 18:01:56 +0530

    Add files via upload

---
+++ build/precache-manifest.c10840db1f379afea09c2bdb781e0b2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a82f97742267db1456626d6ae5ac400",
+    "url": "/To-do-app/index.html"
+  },
+  {
+    "revision": "1673ed58fe607573c25c",
+    "url": "/To-do-app/static/css/main.e8e88b99.chunk.css"
+  },
+  {
+    "revision": "170c4414bacb41027f8a",
+    "url": "/To-do-app/static/js/2.b6ac2b59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-do-app/static/js/2.b6ac2b59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1673ed58fe607573c25c",
+    "url": "/To-do-app/static/js/main.3c948522.chunk.js"
+  },
+  {
+    "revision": "b4f2d459b609166b34d5",
+    "url": "/To-do-app/static/js/runtime-main.7654136d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-do-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
